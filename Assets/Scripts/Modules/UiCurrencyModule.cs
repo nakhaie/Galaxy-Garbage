9c328c05f047d7487e98660771cba20ab9519664
amin.nakhaie <amin.nakhaie@gmail.com>
2019-08-09 19:08:03 +0430

    + Currency HUD

---
+++ Assets/Scripts/Modules/UiCurrencyModule.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Modules
+{
+    public class UiCurrencyModule : MonoBehaviour, IUiCurrencyModule
+    {
+        [SerializeField] private Text nameField;
+        [SerializeField] private Text amountField;
+
+    #region Interface Methods
+        
+        public void SetName(string currencyName)
+        {
+            nameField.text = currencyName;
+        }
+
+        public void SetAmount(int value)
+        {
+            amountField.text = value.ToString();
+        }
+        
+    #endregion
+    }
+    
+    public interface IUiCurrencyModule
+    {
+        void SetName(string currencyName);
+        void SetAmount(int value);
+    }
+}
